1ee54617bee5b331829b334470f820b7f38cd2c0
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-01-29 23:45:37 +0530

    Update 20. Valid Parentheses.cpp

--- 20. Valid Parentheses.cpp
+++ 20. Valid Parentheses.cpp
@@ -1,4 +1,4 @@
-// Using Stack
+// Approach-1 Using Stack
 // T.C : O(N)
 // S.C : O(N)
 class Solution {
@@ -24,6 +24,25 @@ public:
                 if(st.top() == '[') st.pop();
                 else return false;
             }
+        }
+        return st.empty();
+    }
+};
+
+// Approach-2 Using Stack(Tricky)
+// T.C : O(N)
+// S.C : O(N)
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(auto &ch : s){
+            if(ch == '(') st.push(')');
+            else if(ch == '{') st.push('}');
+            else if(ch == '[') st.push(']');
+            else if(st.empty() || st.top() != ch) return false;
+            else st.pop();
         }
         return st.empty();
     }
